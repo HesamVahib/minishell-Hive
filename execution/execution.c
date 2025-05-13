9a55fd2394de49b8e37d75a97ba2ba5be556b877
Minji Choi <michoi@student.hive.fi>
2025-05-13 23:41:38 +0300

    handle additional command error cases

--- execution/execution.c
+++ execution/execution.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execution.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hvahib <hvahib@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: michoi <michoi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 22:01:29 by michoi            #+#    #+#             */
-/*   Updated: 2025/05/11 18:10:04 by hvahib           ###   ########.fr       */
+/*   Updated: 2025/05/13 23:37:04 by michoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int	exec_cmd(t_cmd *cmd_args, t_env *env)
 			CMD_UNSET, NULL};
 	char		**env_arr;
 
+	is_piped_already = false;
 	while (cmd_args)
 	{
 		// if there's only one cmd (no pipe)
@@ -45,20 +46,22 @@ int	exec_cmd(t_cmd *cmd_args, t_env *env)
 				}
 				if (child_pid == 0)
 				{
-					printf("cmd is : %s", cmd_args->argv[0]);
 					env_arr = get_env_arr(env);
 					// close file?
 					if (!env_arr)
 						exit(EXIT_FAILURE);
 					cp.path = get_cmd_path(env, cmd_args->argv[0]);
-					cp.args = cmd_args->argv;
 					if (!cp.path)
 					{
-						printf("no path for execve\n");
+						if (errno == EACCES || errno == ENOENT || errno == EISDIR)
+							print_cmd_err(cmd_args->argv[0], strerror(errno));
+						else
+							print_basic_error(cmd_args->argv[0], "command not found");
 						free_array(&env_arr);
 						exit(EXIT_FAILURE);
 						// close file
 					}
+					cp.args = cmd_args->argv;
 					// execve
 					if (execve(cp.path, cp.args, env_arr))
 					{
