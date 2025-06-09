63a518962d42bd98eafa43ef4e5580191c658309
Minji Choi <michoi@student.hive.fi>
2025-06-09 20:23:22 +0300

    cleanup exit/cd/echo

--- src/builtins/cmd_cd.c
+++ src/builtins/cmd_cd.c
@@ -6,7 +6,7 @@
 /*   By: michoi <michoi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 19:15:43 by michoi            #+#    #+#             */
-/*   Updated: 2025/06/07 14:50:01 by michoi           ###   ########.fr       */
+/*   Updated: 2025/06/09 20:12:01 by michoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,31 +21,6 @@ static int	change_directory(char *path)
 		return (FAILURE);
 	}
 	return (SUCCESS);
-}
-
-static bool	is_valid_path(char *path)
-{
-	struct stat	sb;
-
-	if (stat(path, &sb))
-	{
-		// doesn't exist
-		print_cmd_err_with_arg("cd", path, strerror(ENOENT));
-		return (false); // exit status 1
-	}
-	if (!S_ISDIR(sb.st_mode))
-	{
-		// not a dir
-		print_cmd_err_with_arg("cd", path, strerror(ENOTDIR));
-		return (false); // exit status 1
-	}
-	if (access(path, X_OK))
-	{
-		// needs the execute bit to enter a dir. (permission denied)
-		print_cmd_err_with_arg("cd", path, strerror(EACCES));
-		return (false); // exit status 1
-	}
-	return (true);
 }
 
 static int	change_pwd_vars(t_env *env)
@@ -72,6 +47,50 @@ static int	change_pwd_vars(t_env *env)
 	return (free(old_pwd_cpy), free(new_pwd), SUCCESS);
 }
 
+static int	go_home_dir(t_env *env)
+{
+	char	*home_dir;
+
+	home_dir = find_value_from_env(env, "HOME");
+	if (!home_dir)
+	{
+		print_cmd_err("cd", "HOME not set");
+		return (FAILURE);
+	}
+	if (change_directory(home_dir))
+		return (FAILURE);
+	if (change_pwd_vars(env))
+		return (FAILURE);
+	return (SUCCESS);
+}
+
+/**
+ * 1. when the path doesn't exist
+ * 2. when it's not a directory
+ * 3. permission denied
+ */
+static bool	is_valid_path(char *path)
+{
+	struct stat	sb;
+
+	if (stat(path, &sb))
+	{
+		print_cmd_err_with_arg("cd", path, strerror(ENOENT));
+		return (false);
+	}
+	if (!S_ISDIR(sb.st_mode))
+	{
+		print_cmd_err_with_arg("cd", path, strerror(ENOTDIR));
+		return (false);
+	}
+	if (access(path, X_OK))
+	{
+		print_cmd_err_with_arg("cd", path, strerror(EACCES));
+		return (false);
+	}
+	return (true);
+}
+
 /**
 	Change working directory.
 	@param env: enviroment variables
@@ -83,8 +102,7 @@ static int	change_pwd_vars(t_env *env)
 */
 int	cmd_cd(t_env *env, char **args)
 {
-	int		argc;
-	char	*home_dir;
+	int	argc;
 
 	argc = arrlen(args);
 	if (argc > 1)
@@ -94,15 +112,7 @@ int	cmd_cd(t_env *env, char **args)
 	}
 	if (!argc)
 	{
-		home_dir = find_value_from_env(env, "HOME");
-		if (!home_dir)
-		{
-			print_cmd_err("cd", "HOME not set");
-			return (FAILURE);
-		}
-		if (change_directory(home_dir))
-			return (FAILURE);
-		if (change_pwd_vars(env))
+		if (go_home_dir(env))
 			return (FAILURE);
 		return (SUCCESS);
 	}
@@ -117,9 +127,3 @@ int	cmd_cd(t_env *env, char **args)
 	}
 	return (SUCCESS);
 }
-
-// ☠️ edge cases
-// when parent dir is removed. What to do?
-// rm-rf-> cd . or ..: error retrieving current directory: getcwd: cannot access parent directories: No such file or directory
-// and then add . or .. (dir/. or dir/..)
-// and the if .. is passe again, it goes to home.
--- src/builtins/cmd_exit.c
+++ src/builtins/cmd_exit.c
@@ -6,23 +6,11 @@
 /*   By: michoi <michoi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 21:19:52 by michoi            #+#    #+#             */
-/*   Updated: 2025/06/09 18:09:44 by michoi           ###   ########.fr       */
+/*   Updated: 2025/06/09 20:21:24 by michoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/builtins.h"
-
-static void	print_str_arg_err(char *arg)
-{
-	ft_putendl_fd("exit", STDERR_FILENO);
-	print_cmd_err_with_arg("exit", arg, "numeric argument required");
-}
-
-static void	cleanup_exit(t_env *env, t_cmd *cmd)
-{
-	cleanup_env(env);
-	free_cmd_list(cmd);
-}
 
 static bool	is_numeric(char *input)
 {
@@ -44,6 +32,12 @@ static bool	is_numeric(char *input)
 	return (true);
 }
 
+static void	cleanup_exit(t_env *env, t_cmd *cmd)
+{
+	cleanup_env(env);
+	free_cmd_list(cmd);
+}
+
 /*
 	Exit from its current execution environment with the exit status.
 	specified  by  the  unsigned  decimal integer n
@@ -57,6 +51,8 @@ int	cmd_exit(t_env *env, t_cmd *cmd)
 
 	args = cmd->argv + 1;
 	nbr_range_error = 0;
+	if (!cmd->previous && !cmd->is_piped)
+		ft_putendl_fd("exit", STDERR_FILENO);
 	if (!args || !*args)
 		exit_stat = set_and_get_exit_status(-1, false);
 	else
@@ -64,15 +60,13 @@ int	cmd_exit(t_env *env, t_cmd *cmd)
 		exit_stat = ft_atoll(*args, &nbr_range_error);
 		if ((*args && !is_numeric(*args)) || nbr_range_error)
 		{
-			print_str_arg_err(*args);
+			print_cmd_err_with_arg("exit", *args, "numeric argument required");
 			cleanup_exit(env, cmd);
 			exit(2);
 		}
 		if (is_numeric(*args) && arrlen(args) > 1)
 			return (print_cmd_err("exit", "too many arguments"), FAILURE);
 	}
-	if (!cmd->is_piped && (cmd->previous && !cmd->previous->is_piped))
-		ft_putendl_fd("exit", STDERR_FILENO);
 	cleanup_exit(env, cmd);
 	exit(exit_stat & 255);
 }
